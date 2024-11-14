8bb5540a96cf003942bfb9ce292e43651cd05545
AbdulMoeed <moeedabdul9876@gmail.com>
2024-11-14 13:55:21 +0500

    --

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+
+
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -83,6 +86,7 @@ app.use((err, req, res, next) => {
 });
 
 
-app.listen(3000 , ()=>{
-    console.log("Listening")
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
